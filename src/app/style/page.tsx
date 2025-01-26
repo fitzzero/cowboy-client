b4787fc98b95e305fe4390b000ddfe171ce2270b
Logan <loganfarseer@gmail.com>
2025-01-26 13:15:44 -0800

    style

---
+++ src/app/style/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { Box, ColorPaletteProp, Sheet, Stack, Typography } from '@mui/joy'
+
+const StylePalette = () => {
+  const palettes: ColorPaletteProp[] = [
+    'primary',
+    'neutral',
+    'success',
+    'warning',
+    'danger',
+  ]
+  const weights = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+  return (
+    <>
+      <Typography level='h3'>Primary Palette</Typography>
+      <Stack direction='column' spacing={2}>
+        {palettes.map(palette => (
+          <Box key={palette}>
+            <Typography level='body-lg' color={palette}>
+              {palette}
+            </Typography>
+            <Stack direction='row' spacing={2}>
+              {weights.map(weight => (
+                <Sheet
+                  key={weight}
+                  sx={{
+                    backgroundColor: `${palette}.${weight}`,
+                    color: 'text.primary',
+                    width: 50,
+                    height: 50,
+                  }}>
+                  {weight}
+                </Sheet>
+              ))}
+            </Stack>
+          </Box>
+        ))}
+      </Stack>
+    </>
+  )
+}
+
+export default StylePalette
