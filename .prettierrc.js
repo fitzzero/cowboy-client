df3b0aa7e335904b6ab5e48dec840ef4fdf74405
Logan <loganfarseer@gmail.com>
2025-01-26 12:14:30 -0800

    yarn & prettier init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+};
