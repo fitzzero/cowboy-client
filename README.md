f50820c58256cdd1b78cd83850a5af3faf876e85
Logan <loganfarseer@gmail.com>
2025-02-01 11:30:16 -0800

    staging checkpoint

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Simple Next.js framework for a Prisma.io + Socket.io + Joy UI client
 
 # environment
 
-- `DATABASE_URL`
-- `DISCORD_CLIENT_ID`
-- `DISCORD_CLIENT_SECRET`
-- `NEXTAUTH_SECRET`
-- `NODE_ENV`
+- DATABASE_URL
+- DISCORD_CLIENT_ID
+- DISCORD_CLIENT_SECRET
+- NEXTAUTH_SECRET
+- NODE_ENV
+- NEXTAUTH_URL
+- NODE_ENV
+- SERVER_URL
--- src/components/minecraft/serverStatus.tsx
+++ src/components/minecraft/serverStatus.tsx
@@ -1,27 +1,71 @@
-import { Card, Stack, Typography } from '@mui/joy'
+import { Card, Skeleton, Stack, Typography } from '@mui/joy'
 import { CodeBlock } from '../display/codeBlock'
 import PlaceIcon from '@mui/icons-material/Place'
 import GroupIcon from '@mui/icons-material/Group'
 import UpgradeIcon from '@mui/icons-material/Upgrade'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+interface ServerStatus {
+  online: boolean
+  players: {
+    online: number
+    max: number
+  }
+  version: string
+  motd: {
+    clean: string[]
+  }
+}
 
 export const MinecraftServerStatus = () => {
+  const [status, setStatus] = useState<ServerStatus | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+  const address = 'mc.rally.gg'
+
+  useEffect(() => {
+    const fetchServerStatus = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.mcsrvstat.us/2/${address}`
+        )
+        setStatus(response.data)
+      } catch (err: any) {
+        setError('Failed to fetch server status')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchServerStatus()
+  }, [])
+
   return (
     <Card>
       <Stack direction='column' spacing={1}>
         <Stack direction='row' spacing={1} alignItems='center'>
           <PlaceIcon />
           <Typography>IP:</Typography>
-          <CodeBlock>mc.rally.gg</CodeBlock>
+          <CodeBlock>{address}</CodeBlock>
         </Stack>
         <Stack direction='row' spacing={1} alignItems='center'>
           <UpgradeIcon />
           <Typography>Version:</Typography>
-          <CodeBlock>Java Edition 1.21.4</CodeBlock>
+          <CodeBlock>Java Edition {status?.version}</CodeBlock>
         </Stack>
         <Stack direction='row' spacing={1} alignItems='center'>
           <GroupIcon />
           <Typography>Status:</Typography>
-          <CodeBlock>1/50</CodeBlock>
+          {loading ? (
+            <Skeleton />
+          ) : status?.online ? (
+            <CodeBlock>
+              {status?.players?.online}/{status?.players?.max}
+            </CodeBlock>
+          ) : (
+            <Typography color='danger'>Offline</Typography>
+          )}
         </Stack>
       </Stack>
     </Card>
