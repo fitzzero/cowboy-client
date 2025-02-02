7898268e4bb0cc0416fce4af5a34fc338ffcb294
Logan <loganfarseer@gmail.com>
2025-02-01 19:50:39 -0800

    Fix mobile

--- src/app/minecraftMenu.tsx
+++ src/app/minecraftMenu.tsx
@@ -115,17 +115,19 @@ export const MinecraftMenu = () => {
               </IconButton>
             ) : null}
             {Branding}
-            <Tabs
-              defaultValue={0}
-              onChange={(e, value) => handleChange(value as number)}
-              value={value}
-              sx={{
-                backgroundColor: 'transparent',
-                border: 'none',
-              }}
-              variant='outlined'>
-              {MenuList}
-            </Tabs>
+            {isMobile ? null : (
+              <Tabs
+                defaultValue={0}
+                onChange={(e, value) => handleChange(value as number)}
+                value={value}
+                sx={{
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                }}
+                variant='outlined'>
+                {MenuList}
+              </Tabs>
+            )}
           </>
         }
       />
