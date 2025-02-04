a44893f9b5bef78be20532c70ec2238527a896bb
Logan <loganfarseer@gmail.com>
2025-02-03 17:51:05 -0800

    update map

--- src/app/map/minecraftMap.tsx
+++ src/app/map/minecraftMap.tsx
@@ -1,12 +1,25 @@
+'use client'
+
 import { useEffect, useState } from 'react'
 
 export const MinecraftLiveMap = () => {
   const [src, setSrc] = useState('')
 
-  useEffect(() => {
+  const updateSrc = () => {
     // Generate a unique query parameter to force clear the cache
     const uniqueParam = new Date().getTime()
     setSrc(`https://minecraftmap.techtree.gg/?_=${uniqueParam}`)
+  }
+
+  useEffect(() => {
+    // Initial load
+    updateSrc()
+
+    // Set interval to reload the iframe every 5 minutes (300,000 milliseconds)
+    const intervalId = setInterval(updateSrc, 5 * 60 * 1000)
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId)
   }, [])
 
   return (
